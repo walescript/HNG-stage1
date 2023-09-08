8d3d6983127284dac38d3298ac8cd53331fba2e1
Babalola Adewale <babsonadewale@gmail.com>
2023-09-08 22:16:55 +0100

    second commit

--- Stage_1/main.py
+++ Stage_1/main.py
@@ -7,8 +7,8 @@ app = Flask(__name__)
 @app.route('/info', methods=['GET'])
 def get_info():
     # Retrieve query parameters
-    slack_name = request.args.get('adewale_babson')
-    track = request.args.get('backend')
+    slack_name = request.args.get('slack_name')
+    track = request.args.get('track')
 
     # Get the current day of the week
     current_day = datetime.datetime.utcnow().strftime("%A")
@@ -16,13 +16,19 @@ def get_info():
     # Get the current UTC time as a formatted string
     current_utc_time = datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
 
+    github_file_url = 'https://github.com/walescript/HNG-stage1/blob/main/Stage_1/main.py'
+    github_repo_url = 'https://github.com/walescript/HNG-stage1/tree/main'
+
+
     # Create a response JSON
     response = {
-        "slack_name": slack_name,
-        "track": track,
-        "current_day": current_day,
-        "current_utc_time": current_utc_time,
-        "status_code": 200
+        'slack_name': slack_name,
+        'current_day': current_day,
+        'current_utc_time': current_utc_time,
+        'track': track,
+        'github_file_url': github_file_url,
+        'github_repo_url': github_repo_url,
+        'status_code': 200
     }
 
     return jsonify(response)
